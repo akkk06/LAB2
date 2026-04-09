586e4f57a2c293c77523d1e229bf3014a684624e
akkk06 <leanhkien55@gmail.com>
2026-04-09 22:28:15 +0700

    Update testbench.v

--- LAB2.1/testbench.v
+++ LAB2.1/testbench.v
@@ -1,75 +1,72 @@
-`timescale 1ns / 1ps
+`timescale 1ns/1ps
+module tb_medianfilter;
+    parameter width = 256;
+    parameter height = 256;
+    parameter pixel_cnt = width * height;
 
-module tb_median_filter;
+    reg clk;
+    reg rst_n;
+    reg valid_in;
+    reg [7:0] pixel_in;
+    wire [7:0] pixel_out;
+    wire valid_out;
+    reg [7:0] img_in [0:pixel_cnt-1]; //bo nho luu hoat anh
 
-    // Thay đổi thông số này khớp với kích thước ảnh bạn tạo từ Matlab/Python
-    parameter WIDTH = 256; 
-    parameter HEIGHT = 256;
-    parameter PIXEL_COUNT = WIDTH * HEIGHT;
-
-    // Bộ nhớ lưu ảnh
-    reg [7:0] img_in [0:PIXEL_COUNT-1];
-    
-    // Tín hiệu kết nối với module
-    reg [7:0] p1, p2, p3, p4, p5, p6, p7, p8, p9;
-    wire [7:0] median_val;
-
+    integer read_ptr = 0;
+    integer write_ptr = 0;
     integer file_out;
-    integer i, j;
 
-    // Khởi tạo module lọc
-    lab21 dut(
-        .p1(p1), .p2(p2), .p3(p3),
-        .p4(p4), .p5(p5), .p6(p6),
-        .p7(p7), .p8(p8), .p9(p9),
-        .median(median_val)
+    medianfilter #(
+        width, height
+    ) dut (
+        clk, rst_n, valid_in, pixel_in, valid_out, pixel_out
     );
 
     initial begin
-        // 1. Đọc file ảnh đầu vào (dưới dạng mã HEX)
-        $readmemh("pic_input.txt", img_in);
-        
-        // 2. Mở file để ghi kết quả
-        file_out = $fopen("pic_output.txt", "w");
-
-        $display("Bat dau mo phong loc trung vi...");
-
-        // 3. Quét TẤT CẢ pixel từ 0 đến 255
-        for (i = 0; i < HEIGHT; i = i + 1) begin
-            for (j = 0; j < WIDTH; j = j + 1) begin
-                
-                // Trường hợp 1: Pixel nằm ở rìa ngoài cùng của ảnh -> Xuất thẳng giá trị gốc
-                if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) begin
-                    $fdisplay(file_out, "%02X", img_in[i*WIDTH + j]);
-                end 
-                // Trường hợp 2: Pixel nằm ở lõi ảnh -> Lấy 9 điểm lân cận đưa vào mạch
-                else begin
-                    p1 = img_in[(i-1)*WIDTH + (j-1)];
-                    p2 = img_in[(i-1)*WIDTH + j];
-                    p3 = img_in[(i-1)*WIDTH + (j+1)];
-                    
-                    p4 = img_in[i*WIDTH + (j-1)];
-                    p5 = img_in[i*WIDTH + j];      // Điểm ảnh trung tâm
-                    p6 = img_in[i*WIDTH + (j+1)];
-                    
-                    p7 = img_in[(i+1)*WIDTH + (j-1)];
-                    p8 = img_in[(i+1)*WIDTH + j];
-                    p9 = img_in[(i+1)*WIDTH + (j+1)];
-
-                    // Chờ một chút thời gian để combinational logic tính toán
-                    #10; 
-
-                    // Ghi kết quả lọc ra file
-                    $fdisplay(file_out, "%02X", median_val);
-                end
-                
-            end
-        end
-
-        // 4. Đóng file và kết thúc
-        $fclose(file_out);
-        $display("Hoan tat! Da xuat day du 65536 pixel vao file pic_output.txt");
-        $finish;
+        clk = 0;
+        forever #5 clk = ~clk;
     end
 
+    initial begin
+        $readmemh ("pic_input.txt", img_in);
+        file_out = $fopen ("pic_output.txt", "w");
+        rst_n = 0;
+        valid_in = 0;
+        pixel_in = 0;
+        #20;
+        rst_n = 1;
+        $display("Bat dau dua luong du lieu vao...");
+    end
+
+ always @(posedge clk) begin //bom du lieu vao
+        if (rst_n) begin
+            // B?m ?? s? pixel c?a ?nh + thêm kho?ng tr? ?? ??y data ra (Pipeline Flush)
+            if (read_ptr < pixel_cnt + width + 50) begin
+                
+                if (read_ptr < pixel_cnt) 
+                    pixel_in <= img_in[read_ptr]; // B?m d? li?u th?t
+                else 
+                    pixel_in <= 8'h00;            // B?m pixel ?o ?? ??y d? li?u c? ra
+
+                valid_in <= 1;
+                read_ptr <= read_ptr + 1;
+            end
+            else begin
+                valid_in <= 0;
+            end
+        end
+    end
+
+    always @(posedge clk ) begin //hut du lieu ra
+        if (rst_n && valid_out) begin
+            $fdisplay (file_out, "%02X", pixel_out);
+            write_ptr <= write_ptr + 1;
+            if (write_ptr == pixel_cnt - 1) begin
+                $display ("Hoan tat");
+                $fclose (file_out);
+                #10
+                $finish;
+            end
+        end
+    end
 endmodule
