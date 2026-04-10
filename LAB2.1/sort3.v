878f04cb3a62bd8b351cac7f0648a5ae169d789a
akkk06 <leanhkien55@gmail.com>
2026-04-10 22:32:33 +0700

    h

--- LAB2.1/sort3.v
+++
@@ -1,9 +0,0 @@
-module sort3 (
-		input [7:0] a,b,c,
-		output [7:0] min, med, max
-);
-		wire [7:0] min_ab, max_ab, min_abc;
-		sort2 s1(a, b, min_ab, max_ab);
-		sort2 s2(max_ab, c, min_abc, max); 
-		sort2 s3(min_ab, min_abc, min, med);
-endmodule
